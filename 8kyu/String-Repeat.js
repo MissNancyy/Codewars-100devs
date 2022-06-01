bed4278a47ca93ece82cc1b6cfb66b8ed41c056d
Nancy Jing <nancyjing@Nancys-MBP-4.lan>
2022-05-31 20:25:12 -0700

    added new codewars challenge

---
+++ 8kyu/String-Repeat.js
@@ -0,0 +1,10 @@
+// PROMPT: Write a function called repeatStr which repeats the given string string exactly n times.
+
+// EXAMPLE: 
+//      repeatStr(6, "I") // "IIIIII"
+//      repeatStr(5, "Hello") // "HelloHelloHelloHelloHello"
+
+//MY SOLUTION: 
+function repeatStr (n, s) {
+    return s.repeat(n);
+  }
