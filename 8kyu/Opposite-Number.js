f27dc6d1eefabbb7ca91ecbf5ae13b4904e3e343
Nancy Jing <najing4299@gmail.com>
2022-06-14 14:44:40 -0700

    added new codewars challenge

---
+++ 8kyu/Opposite-Number.js
@@ -0,0 +1,14 @@
+// PROMPT: 
+Very simple, given an integer or a floating-point number, find its opposite.
+
+// EXAMPLE: 
+
+1: -1
+14: -14
+-34: 34 
+
+// MY SOLUTION: 
+function opposite(number) {
+    //your code here
+    return -(number)
+  }
