6b23dce9f28222c43fe592475c74fda061caa929
Nancy Jing <najing4299@gmail.com>
2022-06-17 13:24:04 -0700

    added new codewars challenge

---
+++ 8kyu/Function-2-Squaring-An-Argument.js
@@ -0,0 +1,21 @@
+// PROMPT: Now you have to write a function that takes an argument and returns the square of it.
+
+// MY SOLUTION: 
+function square (argument) {
+    return Math.pow(argument,2)
+  }
+  console.log (square(5)) 
+
+// OR 
+function square (argument) {
+    return argument*argument
+  }
+  console.log (square(5))
+
+// OR
+const square = (n) => n * n; 
+
+// OR 
+function square(x) {
+    return x ** 2;
+  }
