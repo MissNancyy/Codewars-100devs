6933616d2142c214dedf148c1afa310a2454413f
Nancy Jing <najing4299@gmail.com>
2022-06-20 20:46:42 -0700

    added new codewars challenge

---
+++ 8kyu/Make-Uppercase.js
@@ -0,0 +1,16 @@
+// PROMPT: Write a function which converts the input string to uppercase.
+
+// MY SOLUTION:
+function makeUpperCase(str) {
+    // Code here
+    return str.toUpperCase()
+  }
+
+
+// OR 
+
+const makeUpperCase = str => str.toUpperCase(); 
+
+// OR 
+
+function makeUpperCase(str) {return str.toUpperCase();}
