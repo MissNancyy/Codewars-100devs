bdbfd59e6166647c86e4b64ef0ae702d004f907f
Nancy Jing <najing4299@gmail.com>
2022-07-13 21:48:02 -0700

    added new codewars challenge

---
+++ 8kyu/Square(n)-Sum.js
@@ -0,0 +1,39 @@
+// PROMPT:
+Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.
+
+
+// MY SOLUTION:
+function squareSum(numbers){
+    return numbers.reduce(function(sum, n){
+      return (n*n) + sum;
+    }, 0)
+  }
+
+
+// OR
+function squareSum(numbers){
+    var sum = 0;
+    numbers.forEach(function(n) {
+      sum += n * n
+    });
+    return sum;
+  }
+// OR
+function squareSum(numbers){
+    return numbers.reduce((sum,num) => sum + (num * num), 0);
+  }
+
+// OR
+function squareSum(numbers){
+    var result = 0;
+    for (var i = 0; i < numbers.length; i++) {
+      result += (numbers[i] * numbers[i]);
+    }
+    return result;
+  }
+// OR
+function squareSum(numbers) {
+    return numbers.map(n => n ** 2).reduce((a, b) => a + b, 0);
+  }
