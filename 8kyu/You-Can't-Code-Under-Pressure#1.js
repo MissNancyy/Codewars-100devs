e39431a997da01bb589a64657474099d07c0ae7e
Nancy Jing <najing4299@gmail.com>
2022-07-30 17:41:07 -0700

    added new codewars challenge

---
+++ 8kyu/You-Can't-Code-Under-Pressure#1.js
@@ -0,0 +1,18 @@
+// PROMPT:
+Code as fast as you can! You need to double the integer and return it.
+
+
+// MY SOLUTION:
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i * 2;
+  }
+
+// OR
+function doubleInteger(i) {
+    // i will be an integer. Double it and return it.
+    return i+i;
+  }
+
+// OR
+const doubleInteger = i => i*2
