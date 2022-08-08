7996b8241acaa540380f3ccb961af60b413de0f0
Nancy Jing <najing4299@gmail.com>
2022-08-07 19:27:18 -0700

    added new codewars challenge

---
+++ 8kyu/Bin-To-Decimal.js
@@ -0,0 +1,13 @@
+// PROMPT:
+Complete the function which converts a binary number (given as a string) to a decimal number.
+
+// MY SOLUTION:
+function binToDec(bin){
+    return parseInt(bin,2);
+  }
+
+// OR
+const binToDec = bin => parseInt(bin,2);
+
+// OR
+binToDec=(bin)=> parseInt(bin,2)
