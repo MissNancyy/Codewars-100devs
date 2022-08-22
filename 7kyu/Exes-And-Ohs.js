0e14458b281026885e549cc335101a79acb6b18b
Nancy Jing <najing4299@gmail.com>
2022-08-21 23:00:56 -0700

    added new codewars challenge

---
+++ 7kyu/Exes-And-Ohs.js
@@ -0,0 +1,16 @@
+// PROMPT:
+Check to see if a string has the same amount of 'x's and 'o's. The method must return a boolean and be case insensitive. The string can contain any char.
+
+Examples input/output:
+
+XO("ooxx") => true
+XO("xooxx") => false
+XO("ooxXm") => true
+XO("zpzpzpp") => true // when no 'x' and 'o' is present should return true
+XO("zzoo") => false
+// MY SOLUTION:
+function XO(str) {
+    let x = str.match(/x/gi);
+    let o = str.match(/o/gi);
+    return (x && x.length) === (o && o.length);
+  }
